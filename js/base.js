3bc4e1eda01a294f43aadde5b2d9e4f43673ce45
Andrey <andreydavi3ao@gmail.com>
2025-02-10 11:49:49 -0300

    03-Alteração no git

--- js/base.js
+++ js/base.js
@@ -4,3 +4,4 @@ console.log('Olá, sou o console');
 console.log('Eu exibo o conteúdo');
 
 console.log('Alterando o arquivo JS, executando um novo commit');
+console.log('Alterando o arquivo JS, executando um novo commit');
